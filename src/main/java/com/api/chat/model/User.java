193d8d89b1ead0b108c12360c7d94f23ad8643b9
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-27 16:25:09 -0500

    Finished User model(constructors, getters, etc.)

--- src/main/java/com/api/chat/model/User.java
+++ src/main/java/com/api/chat/model/User.java
@@ -14,4 +14,56 @@ public class User {
     private String password;
     @Column
     private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String password, String email) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
