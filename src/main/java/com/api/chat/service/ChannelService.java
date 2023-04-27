8e6518f8716f464a375cc49e2aee81b646992d62
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-27 18:43:49 -0500

    Added autowired

--- src/main/java/com/api/chat/service/ChannelService.java
+++ src/main/java/com/api/chat/service/ChannelService.java
@@ -1,13 +1,17 @@
 package com.api.chat.service;
 
 import com.api.chat.repository.ChannelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChannelService {
     private ChannelRepository channelRepository;
 
+    @Autowired
     public void setChannelRepository(ChannelRepository channelRepository) {
         this.channelRepository = channelRepository;
     }
+
+
 }
