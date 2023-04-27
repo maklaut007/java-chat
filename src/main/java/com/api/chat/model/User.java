3bb637da920d397790bcdb10b69d2f6243ddfdd5
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-27 16:05:47 -0500

    Added users model

--- src/main/java/com/api/chat/model/User.java
+++ src/main/java/com/api/chat/model/User.java
@@ -1,8 +1,19 @@
 package com.api.chat.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String password;
+    @Column
+    private String email;
+
 }
