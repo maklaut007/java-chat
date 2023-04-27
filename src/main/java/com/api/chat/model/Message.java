c5eb1d603bc6c912edd1c4d6369495df7442c494
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-27 16:34:43 -0500

    Added message model

--- src/main/java/com/api/chat/model/Message.java
+++ src/main/java/com/api/chat/model/Message.java
@@ -1,14 +1,18 @@
 package com.api.chat.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "message")
 public class Message {
-
     @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
+    private String text;
+
+    @Column
+    private String channel_id; // connect to channel
 }
