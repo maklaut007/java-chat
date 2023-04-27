9e313704177c007c2ee658af1e151e5dd7770db4
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-27 18:41:44 -0500

    Connected channel repository to service

--- src/main/java/com/api/chat/service/ChannelService.java
+++ src/main/java/com/api/chat/service/ChannelService.java
@@ -1,7 +1,13 @@
 package com.api.chat.service;
 
+import com.api.chat.repository.ChannelRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChannelService {
+    private ChannelRepository channelRepository;
+
+    public void setChannelRepository(ChannelRepository channelRepository) {
+        this.channelRepository = channelRepository;
+    }
 }
