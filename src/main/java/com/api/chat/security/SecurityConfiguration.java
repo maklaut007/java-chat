3e083b3979de0069b635c0ede75165a2f3b6eefd
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-28 18:04:31 -0500

    Connected MyUserDetailsService to SecurityConfiguration

--- src/main/java/com/api/chat/security/SecurityConfiguration.java
+++ src/main/java/com/api/chat/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.api.chat.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -11,6 +12,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/users/register/", "/auth/users/login/").permitAll()
