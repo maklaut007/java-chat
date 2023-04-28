d50993a9d092940e01f93b9644fa4f38d1130231
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-28 16:30:36 -0500

    Added getUserById service

--- src/main/java/com/api/chat/service/UserService.java
+++ src/main/java/com/api/chat/service/UserService.java
@@ -1,5 +1,6 @@
 package com.api.chat.service;
 
+import com.api.chat.model.Channel;
 import com.api.chat.model.User;
 import com.api.chat.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,11 @@ public class UserService {
     public List<User> getUsers() {
         return userRepository.findAll();
     }
-    public User createUser(User user) {
+    public User getUserById(Long user) {
+        return userRepository.findById(user).get();
+    }
+
+    public User createUser(User user) { // !!! check if already exists
         return userRepository.save(user);
     }
 }
