9457d87fc08c44480b737c5301b204a7df61a04c
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-28 17:44:22 -0500

    Created MyUserDetailsService

---
+++ src/main/java/com/api/chat/security/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.api.chat.security;
+
+import com.api.chat.model.User;
+import com.api.chat.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    // Connecting to user service
+    private UserService userService;
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmail(email);
+        return new MyUserDetails(user);
+    }
+}
