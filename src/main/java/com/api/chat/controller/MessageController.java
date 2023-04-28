0b22a0d1c463f1e3cf2f1247d8815d38966a0140
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-27 19:59:00 -0500

    Connected Message controller and service

--- src/main/java/com/api/chat/controller/MessageController.java
+++ src/main/java/com/api/chat/controller/MessageController.java
@@ -1,9 +1,17 @@
 package com.api.chat.controller;
 
+import com.api.chat.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/channel")
 public class MessageController {
+    private MessageService messageService;
+
+    @Autowired
+    public void setMessageService(MessageService messageService) {
+        this.messageService = messageService;
+    }
 }
