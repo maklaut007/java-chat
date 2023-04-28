021c8a9dc2e643a87ec39b1aa364443ecbc6395d
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-28 16:31:48 -0500

    Updated addUserToChannel in service

--- src/main/java/com/api/chat/service/UserChannelService.java
+++ src/main/java/com/api/chat/service/UserChannelService.java
@@ -21,16 +21,22 @@ public class UserChannelService {
     public void setChannelService(ChannelService channelService) {
         this.channelService = channelService;
     }
+    UserService userService;
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
     /**
      * Add selected user to channel with id provided in Url
      * @param channelId id of a channel where we add user
-     * @param user userObject of user that we are adding to a channel
+     * @param userId id of a user that we are adding to a channel
      * @return created UserChannel object
      */
-    public UserChannel addUserToChannel(Long channelId, User user){
+    public UserChannel addUserToChannel(Long channelId, Long userId){
         // Getting channel object
         Channel channel = channelService.getChannelById(channelId);
+        User user = userService.getUserById(userId);
         //Creating object to add into repository
         UserChannel userChannel = new UserChannel(user, channel);
         return userChannelRepository.save(userChannel);
