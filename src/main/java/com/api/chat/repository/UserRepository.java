4c9fd686e0886933635d08833d2e153e4f9166c0
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-28 17:42:51 -0500

    Added findUserByEmail to UserRepository

--- src/main/java/com/api/chat/repository/UserRepository.java
+++ src/main/java/com/api/chat/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.api.chat.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserByEmail(String email);
 }
