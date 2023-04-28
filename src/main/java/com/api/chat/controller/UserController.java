2144dc6520ab1a57fc0393388b8d8cbca9fe4a9e
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-28 14:36:56 -0500

    Added user repository to service

--- src/main/java/com/api/chat/controller/UserController.java
+++ src/main/java/com/api/chat/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/user")
+@RequestMapping(path = "/users")
 class UserController {
 
 }
