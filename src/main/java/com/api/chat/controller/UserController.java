f6ad7871ba7ca2f4e6dd1f63b50edfa4790651c8
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-28 10:15:18 -0500

    Added createMessage to Message service

--- src/main/java/com/api/chat/controller/UserController.java
+++ src/main/java/com/api/chat/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.api.chat.controller;
 
-public class UserController {
+import com.api.chat.model.Channel;
+import com.api.chat.repository.ChannelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/users")
+class UserController {
+
 }
--- src/main/java/com/api/chat/service/MessageService.java
+++ src/main/java/com/api/chat/service/MessageService.java
@@ -10,20 +10,44 @@ import java.util.List;
 
 @Service
 public class MessageService {
+
     // Connect repository to service
     private MessageRepository messageRepository;
     @Autowired
     public void setMessageRepository(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
     }
-    // Get all messages from category
+
+    // Add access to channel service
+    private ChannelService channelService;
+    @Autowired
+    public void setChannelService(ChannelService channelService) {
+        this.channelService = channelService;
+    }
+
     /**
-     * Returns list of messages
+     * Returns list of messages in channel
      * @param channelId id of channel that contains messages
      * @return all messages
      */
     public List<Message> getMessages(Long channelId) {
         return messageRepository.findMessageByChannelId(channelId);
+    }
+
+    /**
+     * Adds new message to channel
+     * @param channelId id of channel that contains messages
+     * @return message object
+     */
+    public Message createMessage(Long channelId, Message message) {
+        Channel channel = channelService.getChannelById(channelId);
+        if(channel != null){
+            message.setChannel(channel);
+            return messageRepository.save(message);
+        } else {
+            // throw error
+        }
+        return messageRepository.save(message); // delete later
     }
     // Get message from category by Id
     // Delete message from category by Id
