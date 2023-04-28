fb8b2817aabd1c8698d122a105994cea56c03235
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-28 15:46:12 -0500

    Find channels by user id in Repository

--- src/main/java/com/api/chat/repository/ChannelRepository.java
+++ src/main/java/com/api/chat/repository/ChannelRepository.java
@@ -2,8 +2,16 @@ package com.api.chat.repository;
 
 import com.api.chat.model.Channel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface ChannelRepository extends JpaRepository<Channel, Long> {
     Channel findChannelById(Long channelId);
+
+    @Query("SELECT uc.channel FROM UserChannel uc  WHERE uc.user.id = :userId")
+    List<Channel> findChannelsByUserId(@Param("userId") Long userId);
+
 }
