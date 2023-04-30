474b7e824b81f25ae82f9081605e47a03fe02be7
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-30 17:35:32 -0500

    Added findUserChannelByUserIdAndChannelId to repository

--- src/main/java/com/api/chat/repository/UserChannelRepository.java
+++ src/main/java/com/api/chat/repository/UserChannelRepository.java
@@ -4,4 +4,5 @@ import com.api.chat.model.UserChannel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserChannelRepository extends JpaRepository<UserChannel, Long> {
+    UserChannel findUserChannelByUserIdAndChannelId(Long userId, Long channelId);
 }
