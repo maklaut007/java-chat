136f4a32f21e42888ae8daca183d4df1b381db48
Maksym Zinchenko <maklaut98@gmail.com>
2023-04-30 22:13:52 -0500

    Added delete user from channel to controller

--- src/main/java/com/api/chat/controller/ChannelController.java
+++ src/main/java/com/api/chat/controller/ChannelController.java
@@ -27,29 +27,37 @@ public class ChannelController {
     public void setUserChannelService(UserChannelService userChannelService) {
         this.userChannelService = userChannelService;
     }
-
+    // http://localhost:8888/channels/
     @GetMapping(path = "/")
     public List<Channel> getChannels() {
         return channelService.getChannels();
     }
 
+    // http://localhost:8888/channels/1/
     @GetMapping(path = "/{channelId}/")
     public Channel getChannelById(@PathVariable Long channelId) {
         return channelService.getChannelById(channelId);
     }
 
+    //http://localhost:8888/channels/
     @PostMapping(path = "/")
     public Channel createChannel(@RequestBody Channel channel) {
         return channelService.createChannel(channel);
     }
 
+    //http://localhost:8888/channels/1/user/1
     @PostMapping(path = "/{channelId}/user/{userId}")
     public UserChannel addUserToChannel(@PathVariable Long channelId, @PathVariable Long userId) {
         return channelService.addUserToChannel(channelId, userId);
     }
+    //http://localhost:8888/channels/1
     @PutMapping(path = "/{channelId}")
     public Channel updateChannel(@PathVariable Long channelId, @RequestBody Channel channel) {
         return channelService.updateChannel(channelId, channel);
     }
-
+    // http://localhost:8888/channels/1
+    @DeleteMapping(path = "/{channelId}")
+    public UserChannel deleteUserFromChannel(@PathVariable Long channelId) {
+        return userChannelService.deleteUserFromChannel(channelId);
+    }
 }
